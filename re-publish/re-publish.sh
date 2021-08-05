7acb5715ab63957102d4b0f32a57204cc655618c
AllalML-FromCyber <allalmohanedlamine@gmail.com>
2021-08-05 18:36:46 +0100

    Refactor naming and pull, push to no local checkout and default to script directory if no local repo is precised

--- re-publish/re-publish.sh
+++ re-publish/re-publish.sh
@@ -1,32 +1,54 @@
 #!/bin/bash
 
-CLINET_REPO=https://github.com/ac-allal/client-repo.git
+# _______ Client Repo
+CLIENT_REPO=https://github.com/ac-allal/client-repo.git
 CLIENT_REPO_REMOTE_NAME=client-remote
 CLIENT_REPO_TARGET_BRANCH=master
 
+# _______ Our Repo
 OUR_REPO=https://git-codecommit.us-east-1.amazonaws.com/v1/repos/testCopy
 OUR_REPO_REMOTE_NAME=origin
 OUR_REPO_LOCAL_PATH=/Users/user203328/Documents/GitHub/TestBranchingCopy
-
-CLINET_REPO_TARGET_BRANCH=master
 PR_BRANCH_TO_PUSH=test
 
 INITIAL_PWD=$PWD
+SCRIPT_DIR=$(dirname "$0")
 
-echo "Check if remote url is set and valid ..."
-cd $OUR_REPO_LOCAL_PATH
-if [ $(git remote get-url $CLIENT_REPO_REMOTE_NAME) = "$CLINET_REPO" ] 
+# _________ Default to current directory. If no our repo local path was set _________
+printf "Setting OUr repo local directory ....\n"
+if [ -z $OUR_REPO_LOCAL_PATH ]
     then
-        echo "setting remote ($CLIENT_REPO_REMOTE_NAME) url to $CLINET_REPO" 
-        if [[ $(git remote get-url $CLIENT_REPO_REMOTE_NAME) == *"No such remote"* ]] 
+        echo "No 'our repo local path' was configured in this script !"
+        echo  "Defaulting to script location directoy (Directory in file system)"
+        if [ -d "$SCRIPT_DIR/.git" ]
             then
-                git remote add $CLIENT_REPO_REMOTE_NAME $CLINET_REPO 
+                echo ".git found > selecting the script current directory"
+                OUR_REPO_LOCAL_PATH=$SCRIPT_DIR
             else
-                git remote set-url $CLIENT_REPO_REMOTE_NAME $CLINET_REPO 
+                echo "The directory is not a git repository (no .git directory found)!"
+                echo "Check the script is in the right git repo. And the repo is init and contains .git folder"
+                echo "Exiting ..."
+                exit
         fi
 fi
 
-# ________________________ branch to push (Dev branch) _______________________
+# ________________________ Check if client repo remote is set _______________________
+echo "Check if client repo remote url is set and valid ..."
+cd $OUR_REPO_LOCAL_PATH
+if [ $(git remote get-url $CLIENT_REPO_REMOTE_NAME) = "$CLIENT_REPO" ] 
+    then
+        echo "setting remote ($CLIENT_REPO_REMOTE_NAME) url to $CLIENT_REPO" 
+        if [[ $(git remote get-url $CLIENT_REPO_REMOTE_NAME) == *"No such remote"* ]] 
+            then # if include 'No such remote' (no remote)
+                git remote add $CLIENT_REPO_REMOTE_NAME $CLIENT_REPO 
+            else
+                git remote set-url $CLIENT_REPO_REMOTE_NAME $CLIENT_REPO 
+        fi
+fi
+
+# ________________________ Set our repo branch to push (Dev PR branch) _______________________
+printf "Set our repo branch to push (Dev PR branch)\n>>>\n"
+
 if [ -z $PR_BRANCH_TO_PUSH ]
     then
         echo "No branch already specfied !"
@@ -48,7 +70,9 @@ fi
 
 printf "\nBranch to push: $PR_BRANCH_TO_PUSH\n\n"
 
-# ________________________ target branch _______________________
+# ________________________ Set Client repo  target branch _______________________
+printf "Set Client repo target branch\n>>>\n"
+
 if [ -z $CLIENT_REPO_TARGET_BRANCH ]
     then
         echo "No target branch was already specfied !"
@@ -69,22 +93,17 @@ if [ -z $CLIENT_REPO_TARGET_BRANCH ]
 fi
 
 # _________________________ Pull remote Target branch ________________________
-printf "\n\nPulling client target remote ($CLIENT_REPO_TARGET_BRANCH) branch\n"
-printf "\nCheckout to $CLIENT_REPO_TARGET_BRANCH\n>>>\n"
-git checkout $CLIENT_REPO_TARGET_BRANCH ||
-(   
-    echo "Branch doesn't exist! We are creating it ..." &&
-    git checkout -b $CLIENT_REPO_TARGET_BRANCH
-)
-
+printf "\n\nPulling client repo target ($CLIENT_REPO_TARGET_BRANCH) branch\n"
 printf "\nPulling .....\n>>>\n"
-git pull $CLIENT_REPO_REMOTE_NAME $CLIENT_REPO_TARGET_BRANCH
+git pull $CLIENT_REPO_REMOTE_NAME $CLIENT_REPO_TARGET_BRANCH:$CLIENT_REPO_TARGET_BRANCH
 
-# _____________ PULL dev branch and push to client remote (for PR) ___________
-printf "\n\nCheckout back to branch to push: $PR_BRANCH_TO_PUSH\n>>>\n"
-git checkout $PR_BRANCH_TO_PUSH
+# _____________ PULL Our repo dev PR branch and push to client remote (for PR) ___________
+printf "\n\nPULL Our repo dev PR branch and push it to client remote (for PR)\n>>>\n"
+
+printf "\nPulling ($PR_BRANCH_TO_PUSH) branch from our repo\n>>>\n"
+git pull $OUR_REPO_REMOTE_NAME $PR_BRANCH_TO_PUSH:$PR_BRANCH_TO_PUSH
+
 printf "\nPush ($PR_BRANCH_TO_PUSH) to client remote\n>>>\n"
-git pull $OUR_REPO_REMOTE_NAME $PR_BRANCH_TO_PUSH
-git push $CLIENT_REPO_REMOTE_NAME $PR_BRANCH_TO_PUSH
+git push $CLIENT_REPO_REMOTE_NAME $PR_BRANCH_TO_PUSH:$PR_BRANCH_TO_PUSH
 
 cd $INITIAL_PWD
