494356b4d1416f96ffa5f74c1b575fec51e0ada6
kitani <slmkitani@gmail.com>
2022-03-17 18:09:16 +0100

    set encoding utf8

--- src/publiser.ts
+++ src/publiser.ts
@@ -3,31 +3,35 @@ import { exit } from 'process';
 import * as fs from 'fs';
 import * as path from 'path';
 
-
 export const publisher = async () => {
   // try {
   // fs.readFile(path.join(__dirname, process.env.TRIGGER_PAYLOAD, 'utf8', (error, data) => {
   //   let webhook_event: string = JSON.parse(data);
   //   console.log("🚀 ~ file: publiser.ts ~ line 25 ~ publisher ~ This means all good from instalattion to variables", {webhook_event})
-  
+
   //   exit();
   // })} catch (error) {
   //   console.log(error)
   // }
 
   try {
-  let rawdata = fs.readFileSync(process.env.TRIGGER_PAYLOAD);
-  
-  let webhook_event = JSON.parse(rawdata);
-  console.log("🚀 ~ file: publiser.ts ~ line 25 ~ publisher ~ This means all good from instalattion to variables", {webhook_event})
+    let rawdata = fs.readFileSync(process.env.TRIGGER_PAYLOAD, {
+      encoding: 'utf8',
+    });
 
-  exit();
-  //   fs.readFile(path.join(__dirname, process.env.TRIGGER_PAYLOAD, 'utf8', (error, data) => {
-  // })
-} catch (error) {
-    console.log(error)
+    let webhook_event = JSON.parse(rawdata);
+    console.log(
+      '🚀 ~ file: publiser.ts ~ line 25 ~ publisher ~ This means all good from instalattion to variables',
+      { webhook_event },
+    );
+
+    exit();
+    //   fs.readFile(path.join(__dirname, process.env.TRIGGER_PAYLOAD, 'utf8', (error, data) => {
+    // })
+  } catch (error) {
+    console.log(error);
   }
-  
+
   // let rawdata = fs.readFileSync(process.env.TRIGGER_PAYLOAD);
   // const args = ['', '', '', '']
   // let request: IRepublishParams;
@@ -50,7 +54,7 @@ export const publisher = async () => {
 
   // await GitService.republish(request);
   // console.log("🚀 ~ file: publiser.ts ~ line 25 ~ publisher ~ This means all good from instalattion to variables", {webhook_event})
-  
+
   // exit();
 };
 
