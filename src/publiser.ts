cdec5db23a289491d17f1cd6b538cd3293c41ade
kitani <slmkitani@gmail.com>
2022-03-17 17:10:21 +0100

    run test function

--- src/publiser.ts
+++ src/publiser.ts
@@ -1,27 +1,34 @@
 import { exit } from 'process';
-import GitService, { IRepublishParams } from './features/git/git.service';
+// import GitService, { IRepublishParams } from './features/git/git.service';
+import * as fs from 'fs';
+
 
 export const publisher = async () => {
-  const args = process.argv.slice(2);
-  let request: IRepublishParams;
+  
+  let rawdata = fs.readFileSync(process.env.TRIGGER_PAYLOAD);
+  let webhook_event = JSON.parse(rawdata);
+  // const args = ['', '', '', '']
+  // let request: IRepublishParams;
 
-  if (args.length !== 3)
-    request = {
-      gitDevRepos:
-        'https://git-codecommit.us-east-1.amazonaws.com/v1/repos/git-republish',
-      developBranch: 'develop',
-      gitMasterRepos: 'git@github.com:react-one/git-republisher.git',
-      masterBranch: 'clone-master',
-    };
-  else
-    request = {
-      gitDevRepos: args[0],
-      developBranch: args[1],
-      gitMasterRepos: args[2],
-      masterBranch: args[3],
-    };
+  // if (args.length !== 3)
+  //   request = {
+  //     gitDevRepos:
+  //       '',
+  //     developBranch: '',
+  //     gitMasterRepos: '',
+  //     masterBranch: '',
+  //   };
+  // else
+  //   request = {
+  //     gitDevRepos: args[0],
+  //     developBranch: args[1],
+  //     gitMasterRepos: args[2],
+  //     masterBranch: args[3],
+  //   };
 
-  await GitService.republish(request);
+  // await GitService.republish(request);
+  console.log("🚀 ~ file: publiser.ts ~ line 25 ~ publisher ~ This means all good from instalattion to variables", {webhook_event})
+  
   exit();
 };
 
